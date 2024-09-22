84f3de97d3b9d21804054863572197fc52fe6e26
KL_jiana <2436262516@qq.com>
2024-09-22 12:46:47 +0800

    复杂

--- src/main/java/com/teampotato/tomato/mixin/MinecraftMixin.java
+++ src/main/java/com/teampotato/tomato/mixin/MinecraftMixin.java
@@ -9,6 +9,8 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Minecraft.class)
--- src/main/java/com/teampotato/tomato/mixin/PauseScreenMixin.java
+++ src/main/java/com/teampotato/tomato/mixin/PauseScreenMixin.java
@@ -2,9 +2,7 @@ package com.teampotato.tomato.mixin;
 
 import net.minecraft.client.gui.screens.PauseScreen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 
 @Mixin(PauseScreen.class)
-public class PauseScreenMixin {
+public class PauseScreenMixin{
 }
